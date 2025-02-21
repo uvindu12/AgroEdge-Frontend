db9ef1ee45b1980d3eb85ad81099c599f9f97e17
devinda.jay <devinda.jay.me@gmail.com>
2025-02-21 22:29:08 +0530

    Added landing page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/ReuseableComponents/Navbar'
+import Hero from './components/ReuseableComponents/Hero'
+
+function App() {
+  return (
+  <>
+  
+      <Navbar />
+      <Hero/>
+       
+  </>   
+  )
+}
+
+export default App
---
+++ src/components/ReuseableComponents/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="sticky top-0 z-10 w-5/6 mx-auto bg-white border-b border-gray-200 rounded-2xl backdrop-filter backdrop-blur-lg bg-opacity-30">
+      <div className="max-w-5xl px-4 mx-auto"> 
+        <div className="flex items-center justify-between h-16">
+          <span className="text-2xl font-semibold text-gray-900"><a href="#"><img className="w-32 h-32" src="src/images/agroedgelogo.png" alt="" /></a></span>
+          <div className="flex justify-center space-x-6 text-gray-900">
+            <a href="#"> Features </a>
+            <a href="#"> Contact </a>
+            <a href="#"> About Us </a>
+          </div>
+          <div> 
+            <a href="https://www.instagram.com/agro_edge_?igsh=MTV0eGZqbXcxbGtqcA%3D%3D&utm_source=qr">
+              <img className="h-7 w-7" src="src/images/instagram logo_icon.svg" alt="Instagram logo" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
