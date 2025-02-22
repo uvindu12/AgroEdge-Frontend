96c8baada3e972e0d1c6163e5969e545c7084011
helithjay <helithjayasuriya77@gmail.com>
2025-02-22 16:06:46 +0530

    (Added server.js again) New start of project.

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const app = require('./app'); //app.js
-const PORT = 5000;
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-// Start the server
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.send("AgroEdge Backend is Running...");
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
