b5df0a7f30c2c6cc744086609471710876fad267
devinda.jay <devinda.jay.me@gmail.com>
2025-02-22 09:42:36 +0530

    Added card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Navbar from './components/ReuseableComponents/Navbar'
 import Hero from './components/ReuseableComponents/Hero'
+import CardHoverEffectDemo from './test'
 
 function App() {
   return (
-  <>
-  
+    <>
       <Navbar />
       <Hero/>
-       
-  </>   
+      <CardHoverEffectDemo/>
+    </>   
   )
 }
 
--- src/components/ReuseableComponents/Hero.jsx
+++ src/components/ReuseableComponents/Hero.jsx
@@ -7,12 +7,12 @@ export default () => {
                 <div className="flex-1 space-y-4 sm:text-center lg:text-left">
                     <h1 className="text-4xl font-bold text-gray-800 xl:text-5xl">
                         Empowering Farmers with
-                         <span className="text-green-600"> Real-Time Insights</span>
+                         <span className="text-green-400"> Real-Time Insights</span>
                     </h1>
                     <p className="max-w-xl leading-relaxed text-gray-500 sm:mx-auto lg:ml-0">
                         AgroEdge transforms traditional farming by providing farmers with real-time data, personalized recommendations, and innovative tools. Our platform empowers farmers to make informed decisions, optimize resources use and boost productivity
                     </p>
-                    <div>
+                    {/* <div>
                         <p className="py-3 text-gray-800">
                             Subscribe to our newsletter and we'll save your time
                         </p>
@@ -26,10 +26,10 @@ export default () => {
                                 Subscribe 
                             </button>
                         </form>
-                    </div>
+                    </div> */}
                 </div>
                 <div className="flex-1 mt-4 text-center lg:mt-0 lg:ml-3">
-                    <img src="https://i.postimg.cc/kgd4WhyS/container.png" className="w-full mx-auto sm:w-10/12 lg:w-full" />
+                    <img src="src/images/hero.jpeg" className="w-full mx-auto sm:w-10/12 lg:w-full" />
                 </div>
             </section>
         </>
