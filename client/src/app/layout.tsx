cd0a78831685b2f3f48b24e46c2680fe8339f935
uvindu <uvindukithmina47@gmail.com>
2025-03-12 12:29:20 +0530

    changed layout code

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Stats from "./components/Stats";
-import Features from "./components/Features";
-import Joinus from "./components/Joinus";
-import { ScrollBasedVelocityDemo } from "./components/Scrollbased";
-
-
-
-
 
 export const metadata: Metadata = {
   title: "AgroEdge",
